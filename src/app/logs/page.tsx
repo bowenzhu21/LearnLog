bca617830535adc330e5efcffccb926459b3d721
Bowen Zhu <bowenzhu66@gmail.com>
2025-10-05 19:55:52 -0400

    edits

--- src/app/logs/page.tsx
+++ src/app/logs/page.tsx
@@ -1,6 +1,8 @@
 import RelayProvider from "@/relay/RelayProvider";
 import LogsView from "./LogsView";
 
+export const dynamic = "force-dynamic";
+
 export default function LogsPage() {
   return (
     <div className="p-8">
